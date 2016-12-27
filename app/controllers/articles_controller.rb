c52ac643f720e94bb543b2f90751b87077a04a07
Abdulmajid <adamsmojeed10@gmail.com>
2016-12-27 17:56:14 +0100

    completed auth session

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
-
+    
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    
     def index
         @articles = Article.paginate(page: params[:page], per_page: 5) #using pagination to list certain amount of articles per page
     end
@@ -10,7 +14,7 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
@@ -20,11 +24,9 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
     end
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:success] = "Article was updated succesfully"
             redirect_to article_path(@article)
@@ -34,14 +36,16 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        @article = Article.find(params[:id])
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:danger] = "Article successfully deleted"
         redirect_to articles_path
+    end
+    
+    def set_article
+        @article = Article.find(params[:id])
     end
     
     private
@@ -49,4 +53,11 @@ class ArticlesController < ApplicationController
         params.require(:article).permit(:title, :description)
     end
     
+    def require_same_user
+        if current_user != @article.user
+            flash[:danger] = "You can only edit or delete your own articles"
+            redirect_to root_path
+        end
+    end
+
 end
